e33f71fbd6ba743525b55ead35250f84c7803dd6
Nahuel Gallinoti <nahuel.gallinoti@incluit.com>
2023-03-30 13:15:20 +0200

    fix ´main´ package json

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import "@angular/compiler";
 import "zone.js";
-import { bootstrapApplication } from "@angular/platform-browser";
+import "./index.css";
 import { AppComponent } from "./app.component";
 import { appRouting } from "./app.routing";
-import "./index.css";
+import { bootstrapApplication } from "@angular/platform-browser";
 
 bootstrapApplication(AppComponent, {
   providers: [...appRouting],
